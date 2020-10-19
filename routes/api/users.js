c3942cc4aa46e9b246819d782189af07f3784a47
Tammy Cuthbert <tammycuthbert427@gmail.com>
2020-10-18 23:00:34 -0500

    const valadate

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,3 +5,6 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const keys = require('../../config/keys');
+
+// We load the input validation from our validation scripts (register & login)
+const validateRegisterInput = require('../../validation/register')
