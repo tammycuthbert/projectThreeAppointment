c086bb5ce678eda56a7156261919bcdf37c3dfc6
Tammy Cuthbert <tammycuthbert427@gmail.com>
2020-10-18 22:27:56 -0500

    update

--- server.js
+++ server.js
@@ -2,3 +2,15 @@ const express = require('express'); // I save the 'express' module in a variable
 const mongoose = require('mongoose'); // I save the 'mongoose' module in a variable so I can refer/use to mongoose functions later on
 const bodyParser = require('body-parser'); // I save the 'body-parser' module in a variable so I can refer/use to body-parser functions later on
 const passport = require('passport');
+
+const users = require('./routes/api/users');
+
+const app = express(); // This express function creates an express application that I save in a variable so I can refer to it later on  
+
+// Body-parser middleware 
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
