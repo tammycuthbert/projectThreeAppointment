2922fa033aea1ef37dcda8d9c12d5618d0a317cc
Tammy Cuthbert <tammycuthbert427@gmail.com>
2020-10-18 22:09:36 -0500

    added pkg.json

--- module.exports = .js
+++ module.exports = .js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:mongodb+srv://tammy_cuthbert:<password>@cluster0.vk9az.mongodb.net/projectThree?retryWrites=true&w=majority,
+    mongoURI:"mongodb+srv://tammy_cuthbert:Giraffe22@cluster0.vk9az.mongodb.net/projectThree?retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
