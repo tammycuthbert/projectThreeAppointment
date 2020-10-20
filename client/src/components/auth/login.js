b24554865d2b3d120696966b8d17f25f717c242c
PolyDevelop <loup.mareau@gmail.com>
2020-10-20 17:58:45 -0500

    Create login.js

---
+++ client/src/components/auth/login.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { loginUser } from '../../actions/authActions';
+
+class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            errors: {}
+        };
+    }
+
+    componentDidMount() {
+        // If the user is logged in and the user navigates to the login page, we redirect them to the dashboard instead
+        if (this.props.auth.isAuthenticated) {
+            this.props.history.push('/dashboard');
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.auth.isAuthenticated) {
+            // When the user logs in successfully, we redirect it to the dashboard
+            this.props.history.push("/dashboard"); 
+        }
+
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors
+            });
+        }
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        // Since we redirect the user within the component (i.e 'componentWillReceiveProps' function), we don't need to pass this.props.history as a parameter
+        this.props.loginUser(userData); 
+    };
+
+    render() {
+        const { errors } = this.state;
+
+        return (
+            <div className="container">
+                <div style={{ marginTop: "4rem" }} className="row">
+                    <div className="col s8 offset-s2">
+                        <Link to="/" className="btn-flat waves-effect">
+                            <i className="material-icons left">keyboard_backspace</i>Back to Home
+                        </Link>
+                        <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                            <h4><b>Login</b> below</h4>
+                            <p className="grey-text text-darken-1">Don't have an account? <Link to="/register/">Register</Link></p>
+                        </div>
+                        <form noValidate onSubmit={ this.onSubmit }>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={ this.onChange }
+                                    value={ this.state.email }
+                                    error={ errors.email }
+                                    id="email"
+                                    type="email"
+                                    className={ classnames("", { invalid: errors.email || errors.emailnotfound }) }
+                                />
+                                <label htmlFor="email">Email</label>
+                                <span className="red-text">
+                                    { errors.email }
+                                    { errors.emailnotfound }
+                                </span>
+                            </div>
+                            <div className="input-field col s12">
+                                <input
+                                    onChange={ this.onChange }
+                                    value={ this.state.password }
+                                    error={ errors.password }
+                                    id="password"
+                                    type="password"
+                                    className={ classnames("", { invalid: errors.password || errors.passwordincorrect }) }
+                                />
+                                <label htmlFor="password">Password</label>
+                                <span className="red-text">
+                                    { errors.password }
+                                    { errors.passwordincorrect }
+                                </span>
+                            </div>
+                            <div className="col s12" style={{ paddingLeft: "11.250px"}}>
+                                <button
+                                    style={{
+                                        width: "150px",
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }}
+                                    type="submit"
+                                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                                >
+                                    Login
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
