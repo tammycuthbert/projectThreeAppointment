7ea0db780df74d00ac6793cf19a03a4d4ade89d3
Tammy Cuthbert <tammycuthbert427@gmail.com>
2020-10-21 21:42:13 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ require('./config/passport')(passport);
 // Routes
 app.use("/api/users", users);
 
-// I setup the port I want to deploy our app on, so I specify Heroku's port here and a default port in case I want to use another port anytime
+// I setup the port I want to deploy my app on, so I specify Heroku's port here and a default port in case I want to use another port anytime
 const port = process.env.PORT || 5000; 
 
 app.listen(port, () => console.log(`Sever up and running on port ${port}!`));
